40556c487d8020cee76ef98c2b22ff1c21ae4d77
Tanvi <tanvi221110@gmail.com>
2024-06-11 20:18:36 +0530

    final edit

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -14,29 +14,28 @@ const Projects = () => {
                         <div className='imgdiv' data-aos="fade-right"
                             data-aos-offset="300"
                             data-aos-easing="ease-in-sine">
-                            <Image alt='#' className='pLeftimg' src="/achieve/flipkart grid.jpg" width={500} height={500}></Image>
+                            <Image alt='#' className='pLeftimg' src="/images/projects/p5.png" width={500} height={500}></Image>
                         </div>
                     </div>
                     <div className='rightdiv'>
                         <div className='pRight'>
                             <div className='githubPart'>
-                                <a href='https://github.com/tanvi1110/Stockers'>
+                                <a href='https://github.com/tanvi1110/Web-Island'>
                                     <RiGithubFill
                                         color="white"
                                         className="my-icon" />
                                 </a>
                             </div>
-                            <div className='paraPart'><b>Stockers: </b> Built the website’s frontend for a stock market prediction platform. Utilized machine learning in a
-                                hackathon to showcase our predictive skills. It is giving accuracy of 86.9% even better than most of predictive tools!</div>
+                            <div className='paraPart'><b>3d-Island: </b>A website that features an integrated 3D model, providing an immersive and interactive user experience. The 3D model allows visitors to explore and interact with it directly on the website, enhancing engagement and offering a dynamic visual element. </div>
                             <div className='lanPart'>
 
-                                <div className='lan'><h5>HTML</h5></div>
+                                <div className='lan'><h5>ViteJS</h5></div>
 
                                 <div className='lan'><h5>CSS</h5></div>
 
-                                <div className='lan'><h5>Python</h5></div>
+                                <div className='lan'><h5>JS</h5></div>
 
-                                <div className='lan'><h5>Streamlit</h5></div>
+                                <div className='lan'><h5>Vercel</h5></div>
                             </div>
                         </div>
                     </div>
@@ -47,29 +46,28 @@ const Projects = () => {
                         <div className='imgdiv' data-aos="fade-right"
                             data-aos-offset="300"
                             data-aos-easing="ease-in-sine">
-                            <Image alt='#' className='pLeftimg' src="/achieve/flipkart grid.jpg" width={500} height={500}></Image>
+                            <Image alt='#' className='pLeftimg' src="/images/projects/p1.png" width={500} height={500}></Image>
                         </div>
                     </div>
                     <div className='rightdiv'>
                         <div className='pRight'>
                             <div className='githubPart'>
-                                <a href='https://github.com/tanvi1110/Stockers'>
+                                <a href='https://github.com/tanvi1110/haven-js'>
                                     <RiGithubFill
                                         color="white"
                                         className="my-icon" />
                                 </a>
                             </div>
-                            <div className='paraPart'><b>Stockers: </b> Built the website’s frontend for a stock market prediction platform. Utilized machine learning in a
-                                hackathon to showcase our predictive skills. It is giving accuracy of 86.9% even better than most of predictive tools!</div>
+                            <div className='paraPart'><b>Haven World: </b>Introducing HavenJS, a newly launched website dedicated to showcasing an exquisite collection of garden items, including flowers, pots, and more. </div>
                             <div className='lanPart'>
 
                                 <div className='lan'><h5>HTML</h5></div>
 
                                 <div className='lan'><h5>CSS</h5></div>
 
-                                <div className='lan'><h5>Python</h5></div>
+                                <div className='lan'><h5>JS</h5></div>
 
-                                <div className='lan'><h5>Streamlit</h5></div>
+                                <div className='lan'><h5>Vercel</h5></div>
                             </div>
                         </div>
                     </div>
@@ -80,29 +78,28 @@ const Projects = () => {
                         <div className='imgdiv' data-aos="fade-right"
                             data-aos-offset="300"
                             data-aos-easing="ease-in-sine">
-                            <Image alt='#' className='pLeftimg' src="/achieve/flipkart grid.jpg" width={500} height={500}></Image>
+                            <Image alt='#' className='pLeftimg' src="/images/projects/p3.png" width={500} height={500}></Image>
                         </div>
                     </div>
                     <div className='rightdiv'>
                         <div className='pRight'>
                             <div className='githubPart'>
-                                <a href='https://github.com/tanvi1110/Stockers'>
+                                <a href='https://github.com/HackTvarit/website'>
                                     <RiGithubFill
                                         color="white"
                                         className="my-icon" />
                                 </a>
                             </div>
-                            <div className='paraPart'><b>Stockers: </b> Built the website’s frontend for a stock market prediction platform. Utilized machine learning in a
-                                hackathon to showcase our predictive skills. It is giving accuracy of 86.9% even better than most of predictive tools!</div>
+                            <div className='paraPart'><b>Hacktvarit: </b> A hackathon website built using Next.js. Hacktvarit serves as a comprehensive platform for organizing and participating in hackathons, offering seamless user experiences and robust functionality. </div>
                             <div className='lanPart'>
 
-                                <div className='lan'><h5>HTML</h5></div>
+                                <div className='lan'><h5>NextJS</h5></div>
 
-                                <div className='lan'><h5>CSS</h5></div>
+                                <div className='lan'><h5>Typescript</h5></div>
 
-                                <div className='lan'><h5>Python</h5></div>
+                                <div className='lan'><h5>Tailwind</h5></div>
 
-                                <div className='lan'><h5>Streamlit</h5></div>
+                                <div className='lan'><h5>Vercel</h5></div>
                             </div>
                         </div>
                     </div>
@@ -113,7 +110,7 @@ const Projects = () => {
                         <div className='imgdiv' data-aos="fade-right"
                             data-aos-offset="300"
                             data-aos-easing="ease-in-sine">
-                            <Image alt='#' className='pLeftimg' src="/achieve/flipkart grid.jpg" width={500} height={500}></Image>
+                            <Image alt='#' className='pLeftimg' src="/images/projects/p4.png" width={500} height={500}></Image>
                         </div>
                     </div>
                     <div className='rightdiv'>
--- src/Components/Walkaway.js
+++ src/Components/Walkaway.js
@@ -10,12 +10,12 @@ const Walkaway = () => {
                 <h3>Walkaway</h3>
             </div>
             <div className='walkawayPart'>
-                <div className='img' data-aos="flip-left" data-aos-duration="3000"><Image alt='#' src="/achieve/flipkart grid.jpg" width={500} height={500}></Image></div>
-                <div className='img' data-aos="flip-left" data-aos-duration="3000"><Image alt='#' src="/achieve/hack the mountain.png" width={400} height={400}></Image></div>
-                <div className='img' data-aos="flip-left" data-aos-duration="3000"><Image alt='#' src="/achieve/hack this fall.png" width={300} height={300}></Image></div>
-                <div className='img' data-aos="flip-left" data-aos-duration="3000"><Image alt='#' src="/achieve/nptel.png" width={200} height={200}></Image></div>
-                <div className='img' data-aos="flip-left" data-aos-duration="3000"><Image alt='#' src="/achieve/hacktoberfest (3).png" width={500} height={500}></Image></div>
-                <div className='img' data-aos="flip-left" data-aos-duration="3000"><Image alt='#' src="/achieve/flipkart grid.jpg" width={400} height={400}></Image></div>
+                <div className='img' data-aos="flip-left" data-aos-duration="3000"><Image alt='#' src="/achieve/w1.png" width={500} height={500}></Image></div>
+                <div className='img' data-aos="flip-left" data-aos-duration="3000"><Image alt='#' src="/achieve/w2.png" width={400} height={400}></Image></div>
+                <div className='img' data-aos="flip-left" data-aos-duration="3000"><Image alt='#' src="/achieve/w3.png" width={300} height={300}></Image></div>
+                <div className='img' data-aos="flip-left" data-aos-duration="3000"><Image alt='#' src="/achieve/w4.png" width={200} height={200}></Image></div>
+                <div className='img' data-aos="flip-left" data-aos-duration="3000"><Image alt='#' src="/achieve/w5.png" width={500} height={500}></Image></div>
+                <div className='img' data-aos="flip-left" data-aos-duration="3000"><Image alt='#' src="/achieve/w6.png" width={400} height={400}></Image></div>
 
 
             </div>
